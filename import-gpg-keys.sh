f3e7e5f12f7a9f82592d85fcd62aa8a73fddcd8f
Branislav Holý <branoholy@gmail.com>
2020-09-21 16:54:24 +0200

    Create import-gpg-keys.sh

---
+++ import-gpg-keys.sh
@@ -0,0 +1,4 @@
+sudo git config --system credential.https://github.com/branoholy/import-gpg-keys.git.helper ""
+git clone --depth 1 https://branoholy@github.com/branoholy/import-gpg-keys.git ~/import-gpg-keys
+
+~/import-gpg-keys/import-gpg-keys/import-gpg-laptop-keys.sh
